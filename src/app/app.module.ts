dc1cb1618b845f093d2fcb4b086764cc8b568fbd
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-04-21 16:07:28 -0300

    agregado bootstrap y menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MenuComponent } from './components/menu/menu.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MenuComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
